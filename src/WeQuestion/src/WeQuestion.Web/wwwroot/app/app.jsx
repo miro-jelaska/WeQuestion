588175a67be22043e2fdcc89e875661164712da6
Miroslav Jelaska <miro@dump.hr>
2016-05-11 09:48:09 +0200

    Added web project

---
+++ src/WeQuestion/src/WeQuestion.Web/wwwroot/app/app.jsx
@@ -0,0 +1,11 @@
+﻿$(function() {
+    var Home = React.createClass({
+        render: function() {
+            return (<h1>Welcome to the Home Pagesd dsf </h1>);
+        }
+    });
+
+    ReactDOM.render((
+        <Home />
+    ), document.getElementById('root'));
+});
