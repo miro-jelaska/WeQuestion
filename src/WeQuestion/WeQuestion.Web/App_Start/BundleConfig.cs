bbca8821e1a39fd28b4a71093d7e90fa6248d58c
Miro Jelaska <git@jelaska.com>
2016-06-24 16:10:43 +0200

    Added Web project

---
+++ src/WeQuestion/WeQuestion.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace WeQuestion.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/libs")
+            .Include(
+                "~/Scripts/libs/modernizr-2.6.2.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/bundles/app")
+            .Include(
+                "~/Scripts/libs/modernizr-2.6.2.js"
+            ));
+        }
+    }
+}
