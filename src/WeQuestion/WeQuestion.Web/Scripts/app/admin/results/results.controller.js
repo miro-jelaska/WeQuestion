ec31ce0d6b5e3f1fe9d152665c595e00e18902c8
Miro Jelaska <git@jelaska.com>
2016-08-21 05:49:41 +0200

    graphs

--- src/WeQuestion/WeQuestion.Web/Scripts/app/admin/results/results.controller.js
+++ src/WeQuestion/WeQuestion.Web/Scripts/app/admin/results/results.controller.js
@@ -8,9 +8,21 @@
         var vm = this;
 
         const surveyId = $stateParams.id;
-
+        vm.labels = ['Corrent', 'wrong', 'Unanswered'];
+        vm.colors = ['#8cbd3f', '#FF2151', '#404040'];
         surveyService.getResult(surveyId)
         .then(result => {
+            console.log(1);
+            result.questionsWithResults = _.map(result.questionsWithResults, function (questionsWithResults) {
+                questionsWithResults.data = [
+                    questionsWithResults.correntAnswersCount,
+                    questionsWithResults.wrongAnswerCount,
+                    questionsWithResults.leftUnansweredCount
+                ];
+                return questionsWithResults;
+            });
+            console.log(result.questionsWithResults);
+
             vm.result = result;
             console.log(result)
         });
--- src/WeQuestion/WeQuestion.Web/Scripts/app/app.module.js
+++ src/WeQuestion/WeQuestion.Web/Scripts/app/app.module.js
@@ -1,5 +1,5 @@
 ﻿(function() {
     'use strict';
 
-    angular.module('app', ['ui.router', 'ui.tree', 'ngDialog', 'angular-clipboard']);
+    angular.module('app', ['ui.router', 'ui.tree', 'ngDialog', 'angular-clipboard', 'chart.js']);
 })();
